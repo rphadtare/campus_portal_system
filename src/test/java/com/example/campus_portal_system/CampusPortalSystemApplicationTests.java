348f0b5f660884b3a176b2659662a249e07b8e38
rphadtare <rohitphadtare39@gmail.com>
2024-04-09 14:43:49 +0200

    2024/04/09 - Campus portal system initial

---
+++ src/test/java/com/example/campus_portal_system/CampusPortalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campus_portal_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusPortalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
