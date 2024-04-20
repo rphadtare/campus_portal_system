1c8c2c6ac995fd6a1bbd0d1ac91b9ccb0a7ecc31
rphadtare <rohitphadtare39@gmail.com>
2024-04-20 15:37:26 +0200

    2024/04/20 - bug fix for admin registration

--- src/main/java/com/example/campus_portal_system/utility/beans/mapper/AdminMapper.java
+++ src/main/java/com/example/campus_portal_system/utility/beans/mapper/AdminMapper.java
@@ -10,23 +10,18 @@ public class AdminMapper implements RowMapper<Admin>  {
     @Override
     public Admin mapRow(ResultSet rs, int rowNum) throws SQLException {
 
-        if(rs.isBeforeFirst()){
-            return new Admin(
-                    rs.getInt("admin_id"),
-                    rs.getInt("institute_id"),
-                    rs.getInt("admin_type_id"),
-                    rs.getString("salutations"),
-                    rs.getString("first_name"),
-                    rs.getString("middle_name"),
-                    rs.getString("last_name"),
-                    rs.getString("qualifications"),
-                    rs.getString("email_id"),
-                    rs.getString("contact_no"),
-                    rs.getInt("is_deleted")
-            );
-        } else {
-            return new Admin(-1);
-        }
+        return new Admin(
+                rs.getInt("admin_id"),
+                rs.getInt("institute_id"),
+                rs.getInt("admin_type_id"),
+                rs.getString("salutations"),
+                rs.getString("first_name"),
+                rs.getString("middle_name"),
+                rs.getString("last_name"),
+                rs.getString("qualifications"),
+                rs.getString("email_id"),
+                rs.getString("contact_no"),
+                rs.getInt("is_deleted"));
 
     }
 }
